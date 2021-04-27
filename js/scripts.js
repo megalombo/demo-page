31b7bffbf129d9978bb6cf998a6edbc87ba56da0
renzobarrionuevo <renzobarrionuevo@gmail.com>
2021-04-27 14:39:04 -0300

    collapse faq's

--- js/scripts.js
+++ js/scripts.js
@@ -41,16 +41,25 @@
         offset: 100,
     });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    //fix para el collapse navbar en ios
+    $(document).scroll(function() {
+    if($(window).scrollTop() !== 0) {
+        $("#mainNav").addClass("navbar-shrink");
+    } else {
+        $("#mainNav").removeClass("navbar-shrink");
+    }
+    });
+
+    // // Collapse Navbar
+    // var navbarCollapse = function () {
+    //     if ($("#mainNav").offset().top > 100) {
+    //         $("#mainNav").addClass("navbar-shrink");
+    //     } else {
+    //         $("#mainNav").removeClass("navbar-shrink");
+    //     }
+    // };
+    // // Collapse now if page is not at top
+    // navbarCollapse();
+    // // Collapse the navbar when page is scrolled
+    // $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
